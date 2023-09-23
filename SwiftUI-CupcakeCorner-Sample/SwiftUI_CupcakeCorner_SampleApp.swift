2e20761136507db12cfb55ec931d8b6ec760074d
ekayaint <ekayaint@gmail.com>
2023-09-23 18:01:36 +0300

    Initial Commit

---
+++ SwiftUI-CupcakeCorner-Sample/SwiftUI_CupcakeCorner_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CupcakeCorner_SampleApp.swift
+//  SwiftUI-CupcakeCorner-Sample
+//
+//  Created by ekayaint on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CupcakeCorner_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
